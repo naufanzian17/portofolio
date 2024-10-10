d07dcbcc18c26fbbf9635d5f5db65216b2390b98
naufal <naufanzian17@gmail.com>
2024-10-10 09:47:20 +0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Jefri Nichol adalah pemeran dan model Indonesia. Namanya dikenal luas berkat perannya sebagai Nathan dalam film Dear Nathan pada tahun 2017.
+Kelahiran: 15 Januari 1999 (usia 25 tahun), Jakarta
+Tinggi: 1,74 m
+Orang tua: John Hendri, Junita Eka Putri
+Saudara kandung: Jessi Putri
+Lahir: Jefry Nikhol; 15 Januari 1999 (umur 25); Jakarta, Indonesia`;
+
+export const ABOUT_TEXT = `Jefri mengawali karier sebagai model iklan dan terjun ke dunia akting dengan menjadi pemeran pendukung dalam miniseri Kami Rindu Ayah pada tahun 2013. Pada tahun 2017, Jefri berperan sebagai Elzan dalam film pertamanya yang berjudul Pertaruhan. Salah satu peran yang mengangkat nama Jefri adalah sebagai Nathan dalam Dear Nathan, yang dirilis pada tahun yang sama.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Actor",
+    company: "Jakarta",
+    description: `Pemeran dan model Indonesia`,
+    technologies: ["Artis", "Aktor", "Model", "Content Creator"],
+  },
+  {
+    year: "2017",
+    role: "Film",
+    company: "Pertaruhan",
+    description: `Pertaruhan merupakan film laga Indonesia yang dirilis pada 9 Februari 2017 yang diproduksi oleh IFI Sinema, disutradarai oleh Krishto Damar Alam dan dibintangi oleh Adipati Dolken, Aliando Syarief, Tio Pakusadewo, Jefri Nichol dan lainnya..`,
+    technologies: ["Artis", "Aktor", "Model", "Content Creator"],
+  },
+  {
+    year: "2022",
+    role: "Film",
+    company: "Jakarta vs Everybody",
+    description: `Jakarta vs Everybody (judul internasional: Jakarta, City of Dreamers) merupakan film yang ditulis dan disutradarai oleh Ertanto Robby Soediskam.[1] Film yang diproduksi oleh Pratama Pradana Picture ini turut menghadirkan sejumlah aktor ternama Indonesia seperti Jefri Nichol, Wulan Guritno, Ganindra Bimo, Jajang C Noer, dan Dea Panendra.[2]
+
+Film ini telah merilis posternya dari salah satu adegan yang menampilkan Jefri Nichol dan Wulan Guritno dengan tagline "Welcome to my city, the place where I can be me."[3] Jakarta vs Everybody berhasil menembus Festival Film Black Nights Tallinn ke-24 (POFF) dan ditayangkan tanggal 26 November 2020 di Estonia..`,
+    technologies: ["Artis", "Aktor", "Model", "Content Creator"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "Film",
+    company: "Ali Topan",
+    description: `Ali Topan adalah film drama romantis perjalanan Indonesia tahun 2023 yang disutradarai oleh Sidharta Tata berdasarkan novel berjudul Ali Topan Anak Jalanan karya Teguh Esha. Film produksi Visinema Pictures serta Legacy Pictures ini dibintangi oleh Jefri Nichol, Lutesha, dan Omara Esteghlal. Ali Topan tayang perdana di Festival Film Internasional Busan pada 7 Oktober 2023.`,
+    technologies: ["Artis", "Aktor", "Model", "Content Creator"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Pemeran Utama Pria Terpuji",
+    image: project1,
+    description:
+      "	Dear Nathan merupakan film drama remaja Indonesia yang dirilis pada 23 Maret 2017 dan disutradarai oleh Indra Gunawan",
+    technologies: ["Amanda Rawles", "Jefri Nichol", "Surya Saputra", "Rayn Wijaya", "Diandra Agatha", "Beby Tsabina", "Denira Wiraguna", "Adi Virsa Prayudi"],
+  },
+  {
+    title: "Pemeran Utama Pria Terbaik",
+    image: project2,
+    description:
+      "Jakarta vs Everybody (judul internasional: Jakarta, City of Dreamers) merupakan film yang ditulis dan disutradarai oleh Ertanto Robby Soediskam.",
+    technologies: ["Jefri Nichol ", "Wulan Guritno", "Ganindra Bimo", "Dea Panendra", "Jajang C. Noer"],
+  },
+  {
+    title: "Best Leading Male Performance - Digital",
+    image: project3,
+    description:
+      "Pertaruhan the Series adalah serial televisi streaming Indonesia produksi Screenplay Films yang ditayangkan perdana 11 Juni 2022 di Vidio berdasarkan film Pertaruhan karya Upi. ",
+    technologies: ["Jefri Nichol", "Giulio Parengkuan", "Clara Bernadeth", "Widika Sidmore", "Abdurrahman Arif"],
+  },
+  {
+    title: "Pemeran Utama",
+    image: project4,
+    description:
+      "Ali Topan adalah film drama romantis perjalanan Indonesia tahun 2023 yang disutradarai oleh Sidharta Tata berdasarkan novel berjudul Ali Topan Anak Jalanan karya Teguh Esha. ",
+    technologies: ["Jefri Nichol", "Lutesha", "Omara Esteghlal", "Reza Hilman", "Bebeto Leutualy"],
+  },
+];
+
+export const CONTACT = {
+  address: "Jakarta, Indonesia ",
+  phoneNo: "+62 882-1822-6224",
+  email: "@jeprinichol",
+};
